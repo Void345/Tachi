4bfa6f61939ca5873ea4b0d29c040a30aab55262
zkldi <20380519+zkldi@users.noreply.github.com>
2022-10-19 23:57:01 +0100

    fix: dont make dropdowns break

--- client/src/components/tables/components/DropdownRow.tsx
+++ client/src/components/tables/components/DropdownRow.tsx
@@ -36,7 +36,7 @@ export default function DropdownRow({
 				{children}
 			</tr>
 			<tr className={nested ? "nested-expandable-pseudo-row" : "expandable-pseudo-row"}>
-				<td colSpan={100}>
+				<td colSpan={100} style={{ padding: 0, borderTop: "none" }}>
 					<Collapse in={showDropdown}>
 						{renderDropdown ? (
 							<div className="tr-dropdown-container">{dropdown}</div>
